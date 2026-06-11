ac801e291a48d37941bd6dbd99416f0880a0f195
Mariam Senzia <mariamsenzia@gmail.com>
2026-06-11 15:55:30 +0300

    feat: add request model

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from models.init import db
+from models.request import Request
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_NOTIFICATION"] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ server/models/request.py
@@ -0,0 +1,23 @@
+from .init import db
+from datetime import datetime
+
+
+class Request(db.Model):
+    __tablename__ = "requests"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    company = db.Column(db.String(100), nullable=False)
+    request_type = db.Column(
+        db.Enum("Bug", "Feature Request", "General Feedback", "Partnership", "Other"),
+        nullable=False,
+    )
+    priority = db.Column(db.Enum("Low", "Medium", "High"), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    status = db.Column(
+        db.Enum("New", "In Review", "Resolved", "Rejected"),
+        default="New",
+        nullable=False,
+    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
