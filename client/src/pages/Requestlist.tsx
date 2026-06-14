e90e461aad0e6ba2e22281fa4b257ba96a895e94
Mariam Senzia <mariamsenzia@gmail.com>
2026-06-14 08:02:56 +0300

    feat: add request table skeleton loader and update text color

--- client/src/pages/Requestlist.tsx
+++ client/src/pages/Requestlist.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Heading,
+  Skeleton,
   Table,
   TableContainer,
   Tbody,
@@ -18,11 +19,15 @@ import AdminNavbar from "../components/reuestlist/AdminNavbar";
 
 const Requestlist = () => {
   const [requests, setRequests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/v1/requests")
       .then((resp) => resp.json())
-      .then((data) => setRequests(data))
+      .then((data) => {
+        setRequests(data);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -60,66 +65,102 @@ const Requestlist = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                {requests.map((req, index) => {
-                  const priorityColor = {
-                    Low: "green",
-                    Medium: "yellow",
-                    High: "red",
-                  };
+                {isLoading
+                  ? Array(5)
+                      .fill(0)
+                      .map((_, index) => {
+                        return (
+                          <Tr key={index}>
+                            <Td>
+                              <Skeleton height="40px" />
+                            </Td>
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>{" "}
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>{" "}
+                            <Td>
+                              <Skeleton height="20px" />
+                            </Td>
+                          </Tr>
+                        );
+                      })
+                  : requests.map((req, index) => {
+                      const priorityColor = {
+                        Low: "green",
+                        Medium: "yellow",
+                        High: "red",
+                      };
 
-                  const statusColor = {
-                    New: "blue",
-                    "In Review": "purple",
-                    Resolved: "green",
-                    Rejected: "red",
-                  };
-                  return (
-                    <Tr key={index}>
-                      <Td>
-                        <VStack alignItems="left">
-                          <Text>{req.name}</Text>
-                          <Text>{req.email}</Text>
-                        </VStack>
-                      </Td>
+                      const statusColor = {
+                        New: "blue",
+                        "In Review": "purple",
+                        Resolved: "green",
+                        Rejected: "red",
+                      };
+                      return (
+                        <Tr key={index} color="#64748b">
+                          <Td>
+                            <VStack alignItems="left">
+                              <Text>{req.name}</Text>
+                              <Text>{req.email}</Text>
+                            </VStack>
+                          </Td>
 
-                      <Td>{req.company}</Td>
-                      <Td>{req.request_type}</Td>
-                      <Td>
-                        <Badge colorScheme={priorityColor[req.priority]}>
-                          {req.priority}
-                        </Badge>
-                      </Td>
-                      <Td maxW="300px" isTruncated>
-                        {req.message}
-                      </Td>
-                      <Td>
-                        <Badge
-                          variant="outline"
-                          colorScheme={statusColor[req.status]}
-                        >
-                          {req.status}
-                        </Badge>
-                      </Td>
-                      <Td>
-                        {new Date(req.created_at).toLocaleDateString("en-GB", {
-                          day: "numeric",
-                          month: "long",
-                          year: "numeric",
-                        })}
-                      </Td>
-                      <Td>
-                        <Button
-                          bg="#2563eb"
-                          color="#fff"
-                          _hover={{ bg: "#1d4ed8" }}
-                          transition="all 0.3s ease"
-                        >
-                          Manage Request
-                        </Button>
-                      </Td>
-                    </Tr>
-                  );
-                })}
+                          <Td>{req.company}</Td>
+                          <Td>{req.request_type}</Td>
+                          <Td>
+                            <Badge colorScheme={priorityColor[req.priority]}>
+                              {req.priority}
+                            </Badge>
+                          </Td>
+                          <Td maxW="300px" isTruncated>
+                            {req.message}
+                          </Td>
+                          <Td>
+                            <Badge
+                              variant="outline"
+                              colorScheme={statusColor[req.status]}
+                            >
+                              {req.status}
+                            </Badge>
+                          </Td>
+                          <Td>
+                            {new Date(req.created_at).toLocaleDateString(
+                              "en-GB",
+                              {
+                                day: "numeric",
+                                month: "long",
+                                year: "numeric",
+                              }
+                            )}
+                          </Td>
+                          <Td>
+                            <Button
+                              bg="#2563eb"
+                              color="#fff"
+                              _hover={{ bg: "#1d4ed8" }}
+                              transition="all 0.3s ease"
+                            >
+                              Manage Request
+                            </Button>
+                          </Td>
+                        </Tr>
+                      );
+                    })}
               </Tbody>
             </Table>
           </TableContainer>
